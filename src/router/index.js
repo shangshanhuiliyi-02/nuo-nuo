a67b789e56231cb3350ccf9b096fe2cd42a295c2
shangshanhuiliyi-02 <939026417@qq.com>
2019-12-10 21:28:39 +0800

    完成登录

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import login from './components/login.Vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,12 +9,35 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    component: login
+    component: Login
+  },
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/Home',
+    name: 'Home',
+    component: Home
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  //     next()  放行    next('/login')  强制跳转
+
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
